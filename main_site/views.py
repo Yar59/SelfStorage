9abb889e11a8cdf1a041b4aa8fc927cb0b09407e
Atmoslayer <ddsomdim@gmail.com>
2023-04-19 21:38:26 +0300

    Render boxes page

--- main_site/views.py
+++ main_site/views.py
@@ -1,27 +1,40 @@
 from django.shortcuts import render
 from .models import Storage, Box, Subscription
-from django.db.models import Count, Max, Min
+from django.db.models import Count, Max
 
 
 def show_main_page(request):
     storage = Storage.objects.annotate(boxes_count=Count('boxes')).order_by('?').first()
-    boxes = storage.boxes.all().annotate(subscriptions_count=Count('subscriptions'))
-    box_max_height = boxes.aggregate(box_max_height=Max('height'))['box_max_height']
-    free_boxes = boxes.filter(subscriptions_count=0).count()
+    free_boxes = storage.boxes.free()
+    box_max_height = free_boxes.aggregate(box_max_height=Max('height'))['box_max_height']
     storage = {
         'address': storage.address,
         'temperature': storage.temperature,
         'max_height': box_max_height,
         'rental_price': storage.rental_price,
         'total_boxes': storage.boxes_count,
-        'free_boxes': free_boxes
+        'free_boxes': free_boxes.count()
     }
 
     return render(request, template_name='index.html', context=storage)
 
 
 def show_boxes(request):
-    return render(request, template_name='boxes.html', context={})
+    storages = Storage.objects.prefetch_related('boxes')
+    context = {'storages': {}}
+    for storage in storages:
+        free_boxes = storage.boxes.free()
+        max_height = free_boxes.aggregate(box_max_height=Max('height'))['box_max_height']
+        context['storages'][storage.pk] = {
+            'info': storage,
+            'free_boxes': free_boxes,
+            'boxes_vol_to_3': free_boxes.filter(height__lt=3),
+            'boxes_vol_to_10': free_boxes.filter(height__lt=10),
+            'boxes_vol_from_10': free_boxes.filter(height__gte=10),
+            'max_height': max_height
+        }
+
+    return render(request, template_name='boxes.html', context=context)
 
 
 def show_my_rent(request):
