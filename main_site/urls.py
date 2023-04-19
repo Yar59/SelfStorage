626c5a7d38a05aa024965308491feb706a29865b
Atmoslayer <ddsomdim@gmail.com>
2023-04-19 19:50:25 +0300

    Attach pages urls

--- main_site/urls.py
+++ main_site/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
+from django.contrib import admin
 from .views import *
 
 app_name = 'main_site'
 
 urlpatterns = [
-    path('', show_main_page, name='index'),
+    path('', show_main_page, name='main'),
+    path('boxes/', show_boxes, name='boxes'),
+    path('my_rent/', show_my_rent, name='my_rent'),
+    path('faq/', show_faq, name='faq'),
 ]
