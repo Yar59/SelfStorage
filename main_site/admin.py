2244b0b8af2a7699eea18aa4f92a294ff2338791
Atmoslayer <ddsomdim@gmail.com>
2023-04-19 18:46:42 +0300

    Register models

--- main_site/admin.py
+++ main_site/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Storage, Box, Subscription
 
-# Register your models here.
+
+@admin.register(Storage)
+class StorageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Box)
+class BoxAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    pass
