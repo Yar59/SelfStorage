98f9470f949cd94accbe2e83c6979db7a97e93ba
Atmoslayer <ddsomdim@gmail.com>
2023-04-20 22:03:11 +0300

    Create ImageInline with thumbnail

--- main_site/admin.py
+++ main_site/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
-from .models import Storage, Box, Subscription
+from django.utils.html import format_html
+
+from .models import Storage, Box, Subscription, Image
+
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    readonly_fields = ['place_images']
+    extra = 0
+    def place_images(self, image_object):
+
+        return format_html('<img src="{}" height=200px />', image_object.image.url)
+
+    place_images.short_description = 'Миниатюра'
 
 
 @admin.register(Storage)
 class StorageAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ImageInline]
 
 
 @admin.register(Box)
@@ -15,3 +28,9 @@ class BoxAdmin(admin.ModelAdmin):
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
