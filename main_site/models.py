da0b4cffd5c2f8a58a2774db078acd6ae07ccb4d
Atmoslayer <ddsomdim@gmail.com>
2023-04-21 20:18:18 +0300

    Increase address max length for the Storage model

--- main_site/models.py
+++ main_site/models.py
@@ -53,7 +53,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class Storage(models.Model):
     name = models.CharField('Название', max_length=20)
-    address = models.CharField('Адрес', max_length=30)
+    address = models.CharField('Адрес', max_length=100)
     temperature = models.DecimalField('Температура на складе', decimal_places=2, max_digits=5)
     rental_price = models.DecimalField('Цена аренды', decimal_places=2, max_digits=8)
     note = models.CharField(null=True, blank=True, max_length=35, verbose_name='Заметка')
