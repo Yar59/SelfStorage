d3072e48455bc9e406af33b67857435c666959a7
Yar59 <75260156+Yar59@users.noreply.github.com>
2023-04-23 18:39:42 +0300

    correct previos commit

--- main_site/management/commands/__init__.py
+++ main_site/management/commands/__init__.py
@@ -1,12 +1,1 @@
-from django.core.management import BaseCommand
-from django.conf import settings
-
-from main_site.mailer import *
-
-
-class Command(BaseCommand):
-    help = 'Запустите для рассылки сообщений на почту'
-
-    def handle(self, *args, **kwargs):
-        pass
 
